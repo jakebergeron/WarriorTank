b5627ff136c8a6068583c326fa69e069f17b1056
jake.bergeron <jake.bergeron>
2025-08-23 10:43:00 -0400

    Update warriorTank.lua: improve functionality and fix minor issues

--- warriorTank.lua
+++ warriorTank.lua
@@ -1,14 +1,19 @@
 WarriorTank = {};
-function WarriorTank_OnLoad()
-	this:RegisterEvent("PLAYER_ENTERING_WORLD");
-	this:RegisterEvent("ADDON_LOADED");
-	DEFAULT_CHAT_FRAME:AddMessage("WarriorTank addon loaded. Type /tank for usage.");
-	SlashCmdList["WARRIORTANK"] = function()
-		local msg = "To use WarriorTank addon, create a macro and type /script WarriorTank_main();"
-		DEFAULT_CHAT_FRAME:AddMessage(msg);
-	end;
-	SLASH_WARRIORTANK1 = "/tank";
-end;
+
+local WarriorTankFrame = CreateFrame("Frame")
+WarriorTankFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+WarriorTankFrame:RegisterEvent("ADDON_LOADED")
+
+WarriorTankFrame:SetScript("OnEvent", function(self, event, ...)
+	if event == "ADDON_LOADED" then
+		DEFAULT_CHAT_FRAME:AddMessage("WarriorTank addon loaded. Type /tank for usage.")
+		SlashCmdList["WARRIORTANK"] = function()
+			local msg = "To use WarriorTank addon, create a macro and type /script WarriorTank_main();"
+			DEFAULT_CHAT_FRAME:AddMessage(msg)
+		end
+		SLASH_WARRIORTANK1 = "/tank"
+	end
+end)
 
 function WarriorTank_main()	
 	local msNameTalent, msIcon, msTier, msColumn, msCurrRank, msMaxRank = GetTalentInfo(1,18);
@@ -25,15 +30,17 @@ function WarriorTank_main()
 	local revengeTexture = "Ability_Warrior_Revenge";
 	local mainCost = 30;
 	local impSunderNameTalent, impSunderIcon, impSunderTier, impSunderColumn, impSunderCurrRank, impSunderMaxRank = GetTalentInfo(3,10);
+	if impSunderCurrRank == nil then impSunderCurrRank = 0 end
 	local sunderRage = 15 - impSunderCurrRank;
 	
 	if(mainDamage == "Shield Slam") then mainCost = 20; end;
-	
 	for i = 1, 5, 1
 		do
-			if(WarriorTank_getSpellId(abilities[i]) ~= nil) then
-				ids[i] = WarriorTank_getSpellId(abilities[i]);
+			local spellId = WarriorTank_getSpellId(abilities[i]);
+			if(spellId ~= nil) then
+				ids[i] = spellId;
 			end;
+	end;
 	end;
 	
 	-- Get spell cooldowns safely
@@ -47,8 +54,12 @@ function WarriorTank_main()
 
 	local sbStart, sbDuration, sbEnabled = safeGetSpellCooldown(ids[1])
 	local mainStart, mainDuration, mainEnabled = safeGetSpellCooldown(ids[5])
-	local reveStart, revDuration, revEnabled = safeGetSpellCooldown(ids[2])
-	local revengeUsable = IsUsableAction(WarriorTank_findActionSlot(revengeTexture));
+	local revStart, revDuration, revEnabled = safeGetSpellCooldown(ids[2])
+	local revengeSlot = WarriorTank_findActionSlot(revengeTexture);
+	local revengeUsable = 0;
+	if revengeSlot ~= 0 then
+		revengeUsable = IsUsableAction(revengeSlot);
+	end
 	
 	--if shield block not active and not on cd then shield block
 	if (WarriorTank_isBuffTextureActive(sbTexture) == false and sbDuration == 0 and rage >= 10) then cast(abilities[1]);
@@ -69,7 +80,7 @@ function WarriorTank_getSpellId(spell)
 	while true do
 	   local spellName, spellRank = GetSpellName(i, BOOKTYPE_SPELL)
 	   if not spellName then
-		  do break end
+		  break
 	   end
 	   if spellName == spell then
 	   return i; end;
@@ -80,21 +91,24 @@ end;
 function WarriorTank_isBuffTextureActive(texture)
 	local i=0;
 	local g=GetPlayerBuff;
-	local isBuffActive = false;
+		local buffTexture = GetPlayerBuffTexture(g(i));
+		if(buffTexture ~= nil and string.find(buffTexture, texture)) then isBuffActive = true; end;
 
 	while not(g(i) == -1)
 	do
-		if(strfind(GetPlayerBuffTexture(g(i)), texture)) then isBuffActive = true; end;
+		if(string.find(GetPlayerBuffTexture(g(i)), texture)) then isBuffActive = true; end;
 		i=i+1
 	end;	
 	return isBuffActive;
 end;
-
 function WarriorTank_findActionSlot(spellTexture)	
 	for i = 1, 120, 1
 		do
-		if(GetActionTexture(i) ~= nil) then 
-		if(strfind(GetActionTexture(i), spellTexture)) then return i; end; end;
+		local actionTexture = GetActionTexture(i)
+		if actionTexture ~= nil then 
+			if(string.find(actionTexture, spellTexture)) then return i; end;
+		end;
 	end;
 	return 0;
+end;
 end;
